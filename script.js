159655e15c6007f832fb381e474a1da0621852b2
Maria Casby <mariaallen@comcast.net>
2020-03-04 22:47:50 -0500

    completed 3rd page

--- script.js
+++ script.js
@@ -9,8 +9,10 @@ $(document).ready(function () {
         $("#start").hide();
         $(myDiv).show(1000);
         $('#part2').removeClass('display');
+
         $("#myCounter").text(counter);
         myTimer();
+
     });
 
     $("li").mouseenter(function () {
@@ -22,88 +24,105 @@ $(document).ready(function () {
 
 
     $("li").click(function () {
-        // `this` is the DOM element that was clicked
+
         var index = $("li").index(this);
-       // $("span").text("You clicked answer #" + index);
+
         if (index == currentQuestion[4]) {
             $("h4").text("correct");
             $("#myCounter").text(++counter);
+  
         } else {
             $("h4").text("wrong");
         }
 
     });
 
+
 });
+    var questions = new Array();
+    questions[0] = new Array("Name the capital of Afghanistan", "Kabul", "Baku", "Tirana", 0);
+    questions[1] = new Array("Name the capital of Poland", "Prague", "Warsaw", "Bucharest", 1);
+    questions[2] = new Array("Name the capital of Belgium", "London", "Sofia", "Brussels", 2);
+    questions[3] = new Array("Name the capital of Burkina Faso", "Gaborone", "Niamey", "Ouagadougou", 2);
+    questions[4] = new Array("Name the capital of Pakistan", "Islamabad", "Kinshasa", "Manila", 0);
+    questions[5] = new Array("Name the capital of Colombia", "Muscat", "Bogota", "Brussels", 1);
+    questions[6] = new Array("Name the capital of Poland", "Prague", "Warsaw", "Bucharest", 1);
+    questions[7] = new Array("Name the capital of Belgium", "London", "Sofia", "Brussels", 2);
+    questions[8] = new Array("Name the capital of Burkina Faso", "Gaborone", "Niamey", "Ouagadougou", 2);
+    questions[9] = new Array("Name the capital of Pakistan", "Islamabad", "Kinshasa", "Manila", 0);
+    questions[10] = new Array("Name the capital of Colombia", "Muscat", "Bogota", "Brussels", 1);
 
-var questions = new Array();
-questions[0] = new Array("Name the capital of Afghanistan", "Kabul", "Baku", "Tirana", 0);
-questions[1] = new Array("Name the capital of Poland", "Prague", "Warsaw", "Bucharest", 1);
-questions[2] = new Array("Name the capital of Belgium", "London", "Sofia", "Brussels", 2);
-questions[3] = new Array("Name the capital of Burkina Faso", "Gaborone", "Niamey", "Ouagadougou", 2);
-questions[4] = new Array("Name the capital of Pakistan", "Islamabad", "Kinshasa", "Manila", 0);
-questions[5] = new Array("Name the capital of Colombia", "Muscat", "Bogota", "Brussels", 1);
-questions[6] = new Array("Name the capital of Poland", "Prague", "Warsaw", "Bucharest", 1);
-questions[7] = new Array("Name the capital of Belgium", "London", "Sofia", "Brussels", 2);
-questions[8] = new Array("Name the capital of Burkina Faso", "Gaborone", "Niamey", "Ouagadougou", 2);
-questions[9] = new Array("Name the capital of Pakistan", "Islamabad", "Kinshasa", "Manila", 0);
-questions[10] = new Array("Name the capital of Colombia", "Muscat", "Bogota", "Brussels", 1);
 
-
-var myQ = document.createElement("p");
-var myDiv = document.createElement("div");
-var myOL = document.createElement("ol");
-var myLI1 = document.createElement("li");
-var myLI2 = document.createElement("li");
-var myLI3 = document.createElement("li");
-var currentQuestion = questions[0];
-myQ.textContent = currentQuestion[0];
-myLI1.textContent = currentQuestion[1];
-myLI2.textContent = currentQuestion[2];
-myLI3.textContent = currentQuestion[3];
-myOL.appendChild(myLI1);
-myOL.appendChild(myLI2);
-myOL.appendChild(myLI3);
-myDiv.appendChild(myQ);
-myDiv.appendChild(myOL);
-//$("#main").append(myDiv);
-document.getElementById("main").appendChild(myDiv);
-
-var i = 0;
-var nextQuestion = function () {
-
-    i++;
-    currentQuestion = questions[i];
+    var myQ = document.createElement("p");
+    var myDiv = document.createElement("div");
+    var myOL = document.createElement("ol");
+    var myLI1 = document.createElement("li");
+    var myLI2 = document.createElement("li");
+    var myLI3 = document.createElement("li");
+    var currentQuestion = questions[0];
     myQ.textContent = currentQuestion[0];
     myLI1.textContent = currentQuestion[1];
     myLI2.textContent = currentQuestion[2];
     myLI3.textContent = currentQuestion[3];
-}
+    myOL.appendChild(myLI1);
+    myOL.appendChild(myLI2);
+    myOL.appendChild(myLI3);
+    myDiv.appendChild(myQ);
+    myDiv.appendChild(myOL);
+    //$("#main").append(myDiv);
+    document.getElementById("main").appendChild(myDiv);
+
+    var i = 0;
+    var nextQuestion = function () {
+
+        i++;
+        currentQuestion = questions[i];
+        myQ.textContent = currentQuestion[0];
+        myLI1.textContent = currentQuestion[1];
+        myLI2.textContent = currentQuestion[2];
+        myLI3.textContent = currentQuestion[3];
+    }
 
 
 
+var myName;
 
+    var myTimer = function () {
+        var counters = 5;
 
-var myTimer = function () {
-    var counters = 10;
+        setInterval(function () {
+            counters--;
+            if (counters >= 0) {
+                span = document.getElementById("count");
+                span.innerHTML = counters;
+            }
 
-    setInterval(function () {
-        counters--;
-        if (counters >= 0) {
-            span = document.getElementById("count");
-            span.innerHTML = counters;
-        }
- 
-        if (counters === 0) {
+            if (counters === 0) {
+
+                clearInterval(counters);
+                span.innerHTML = `Time is Over. Your score is ${counter}`
+                $(myDiv).hide(1000);
+                $('#part2').hide(1000);
+                $('#part3').removeClass('nodisplay');
+                $('#part3').addClass('display1');
+                $('#result').text(counter);
+                $("#nameButton").click(function () {
+                    var value = $('input').val()
+                    $("#displayname").text(value);
+                 
     
-            clearInterval(counters);
-            span.innerHTML =`Time is Over. Your score is ${counter}`
-            $(myDiv).hide(1000);
-        }
+                  //return myName = $("input:text").val("type your first name");
+                 // event.preventDefault();
+                    
+                   });
+                   keyup();
+                  // console.log(myName);
+            }
 
-    }, 1000);
+        }, 1000);
 
-};
+       
+    };
 
 
 
